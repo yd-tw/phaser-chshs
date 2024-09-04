3a4053bd16ee29c9fb32a9f67cb1876273e33f73
yd-tw <yd960528@gmail.com>
2024-09-04 23:22:36 +0800

    Update value name

--- game.js
+++ game.js
@@ -1,38 +1,38 @@
-const config = {
+const gameConfig = {
   type: Phaser.AUTO,
   scale: {
     mode: Phaser.Scale.RESIZE,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     parent: 'game-container',
     width: '100%',
-    height: '100%'
+    height: '100%',
   },
   physics: {
     default: 'arcade',
     arcade: {
-      debug: false
-    }
+      debug: false,
+    },
   },
   scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+    preload: preloadAssets,
+    create: createGameObjects,
+    update: updatePlayerMovement,
+  },
 };
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(gameConfig);
 
 let player;
 let cursors;
-let platforms;
+let buildings;
 
-function preload() {
+function preloadAssets() {
   this.load.image('background', 'assets/background.jpg');
-  this.load.image('house', 'assets/house.png');
-  this.load.image('character', 'assets/character.jpg');
+  this.load.image('building', 'assets/house.png');
+  this.load.image('player', 'assets/player.jpg');
 }
 
-function create() {
+function createGameObjects() {
   const { width, height } = this.scale;
 
   // 添加背景
@@ -40,26 +40,26 @@ function create() {
   background.setDisplaySize(width, height);
 
   // 建立靜態平台群組
-  platforms = this.physics.add.staticGroup();
+  buildings = this.physics.add.staticGroup();
 
-  // 添加地面和平台，並為每個平台設置名稱
-  const ground1 = platforms.create(400, 568, 'house').setName('house1');
-  const ground2 = platforms.create(600, 400, 'house').setName('house2');
+  // 添加房子，並為每個房子設置名稱
+  const house1 = buildings.create(400, 568, 'building').setName('house1');
+  const house2 = buildings.create(600, 400, 'building').setName('house2');
 
   // 添加玩家角色
-  player = this.physics.add.sprite(100, 450, 'character');
-  player.setBounce(0.2); // 設定彈跳
+  player = this.physics.add.sprite(100, 450, 'player');
+  player.setBounce(0.2); // 設定彈跳效果
   player.setCollideWorldBounds(true); // 防止超出邊界
 
-  // 添加玩家與平台間的碰撞
-  this.physics.add.collider(player, platforms, onPlayerCollide, null, this);
+  // 添加玩家與房子間的碰撞
+  this.physics.add.collider(player, buildings, onPlayerCollideWithHouse, null, this);
 
   // 設定鍵盤輸入
   cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {
-  // 更新玩家移動
+function updatePlayerMovement() {
+  // 玩家移動邏輯
   if (cursors.left.isDown) {
     player.setVelocityX(-160);
   } else if (cursors.right.isDown) {
@@ -68,7 +68,6 @@ function update() {
     player.setVelocityX(0);
   }
 
-  // 設定玩家跳躍
   if (cursors.up.isDown) {
     player.setVelocityY(-160);
   } else if (cursors.down.isDown) {
@@ -78,24 +77,25 @@ function update() {
   }
 }
 
-// 玩家碰撞事件處理函數
-function onPlayerCollide(player, ground) {
-  if (ground.name === 'house1') {
-    // 顯示介面或執行ground1相關的動作
+// 玩家碰撞房子的事件處理函數
+function onPlayerCollideWithHouse(player, building) {
+  if (building.name === 'house1') {
+    // 顯示介面或執行house1相關的動作
     console.log('Player hit house1');
-    showInterface1();
-  } else if (ground.name === 'house2') {
-    // 顯示介面或執行ground2相關的動作
+    showHouse1Interface();
+  } else if (building.name === 'house2') {
+    // 顯示介面或執行house2相關的動作
     console.log('Player hit house2');
-    showInterface2();
+    showHouse2Interface();
   }
 }
 
-// 定義不同的顯示介面函數
-function showInterface1() {
-  alert('玩家碰到了 ground1');
+// 顯示house1的介面
+function showHouse1Interface() {
+  alert('玩家碰到了 house1');
 }
 
-function showInterface2() {
-  alert('玩家碰到了 ground2');
+// 顯示house2的介面
+function showHouse2Interface() {
+  alert('玩家碰到了 house2');
 }
