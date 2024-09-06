b31b769589b1cbcf9c4930bc64a189bf0251b5c3
yd-tw <yd960528@gmail.com>
2024-09-06 20:17:00 +0800

    Add more scence

--- src/House1Scene.js
+++ src/House1Scene.js
@@ -10,8 +10,10 @@ export default class House1Scene extends Phaser.Scene {
   }
 
   create() {
+    const { width, height } = this.scale;
     // 設置場景背景或其他場景元素
-    this.add.image(0, 0, 'house1Background').setOrigin(0, 0);
+    const background = this.add.image(0, 0, 'house1Background').setOrigin(0, 0);
+    background.setDisplaySize(width, height);
     // 添加返回到主場景的按鈕或鍵盤事件
     this.input.keyboard.on('keydown-ESC', () => {
       this.scene.start('MainScene'); // 返回到主場景
